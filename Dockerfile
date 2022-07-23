598262d30c42d3d54e6d9bcf11c09e3b55c983ca
Dmitry <001122mj@gmail.com>
2022-07-24 00:40:00 +0300

    rels #6. Finally dockerize

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . /bot
 
-# CMD ["python", "app.py"]
+CMD ["python", "app.py"]
--- db/database.py
+++ db/database.py
@@ -1,13 +1,18 @@
 from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from data import config
 
-engine = create_engine(config.DATABASE_URI)
-Session = sessionmaker(bind=engine)
+
+db_engine = create_engine(config.DATABASE_URI)
+if not database_exists(db_engine.url):
+    create_database(db_engine.url)
+
+Session = sessionmaker(bind=db_engine)
 Base = declarative_base()
 
 
 def create_db():
-    Base.metadata.create_all(engine)
+    Base.metadata.create_all(db_engine)
