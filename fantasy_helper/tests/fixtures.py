c0864041f1dbfe790c5159d641be5644d949d4f9
se4en <001122mj@gmail.com>
2023-07-22 00:22:00 +0300

    #9. Add basic test for xbet parser

---
+++ fantasy_helper/tests/fixtures.py
@@ -0,0 +1,17 @@
+from typing import List
+import os.path as path
+
+from pytest import fixture
+from hydra import compose, initialize
+from hydra.utils import instantiate
+
+from fantasy_helper.utils.dataclasses import LeagueInfo
+
+
+@fixture
+def leagues() -> List[LeagueInfo]:
+    # load leagues info
+    initialize(config_path="../conf", version_base=None)
+    cfg = compose(config_name="config")
+    leagues = instantiate(cfg.leagues)
+    yield leagues
