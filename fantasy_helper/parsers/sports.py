5f68a9ba29a6b30d1240473268671781500d294e
se4en <001122mj@gmail.com>
2023-07-22 11:43:28 +0300

    #9. Update sports parser

--- fantasy_helper/parsers/sports.py
+++ fantasy_helper/parsers/sports.py
@@ -1,6 +1,5 @@
-import typing as t
+from typing import Dict, Any, List, Literal, Optional
 import os
-from datetime import datetime
 import logging
 import json
 
@@ -13,9 +12,9 @@ from fantasy_helper.utils.dataclasses import LeagueInfo
 class SportsParser:
     def __init__(
         self,
-        leagues: t.List[LeagueInfo],
+        leagues: List[LeagueInfo],
         url: str = "https://www.sports.ru/gql/graphql/",
-        queries_path: t.Optional[str] = None,
+        queries_path: Optional[str] = None,
     ):
         self.__url = url
         if queries_path is not None:
@@ -26,8 +25,8 @@ class SportsParser:
         self.__leagues = {l.name: l.squad_id for l in leagues if l.squad_id is not None}
 
     def __get_query_body(
-        self, query_name: t.Literal["squad", "tournament"]
-    ) -> t.Optional[str]:
+        self, query_name: Literal["squad", "tournament"]
+    ) -> Optional[str]:
         fname = os.path.join(self.__queries_path, f"{query_name}.graphql")
 
         if os.path.exists(fname):
@@ -38,8 +37,8 @@ class SportsParser:
             return None
 
     def __request_for_squad_id(
-        self, squad_id: int, query_name: t.Literal["squad", "tournament"]
-    ) -> t.Optional[dict]:
+        self, squad_id: int, query_name: Literal["squad", "tournament"]
+    ) -> Optional[dict]:
         headers = {"Content-Type": "application/json"}
         body = self.__get_query_body(query_name)
         if body is None:
@@ -59,53 +58,35 @@ class SportsParser:
             # TODO log bad response for graphql
             return None
 
-    def __get_current_tour(self, squad_id: int) -> t.Optional[dict]:
+    def __get_current_tour(self, squad_id: int) -> Optional[dict]:
         data = self.__request_for_squad_id(squad_id, "tournament")
         if data is None:
             return None
 
         # looking for current tour
-        query_id = data["data"].keys()[0]
+        query_id = list(data["data"].keys())[0]
         for tour in data["data"][query_id]["squads"][0]["season"]["tours"]:
             if tour["status"] == "OPENED":
                 break
 
         return tour
 
-    def get_cur_tour(self, league_name: str) -> t.Optional[int]:
+    def get_cur_tour_info(self, league_name: str) -> Optional[Dict[str, Any]]:
         if league_name not in self.__leagues:
             logging.info(f"Wrong league_name={league_name} in get_deadline")
             return None
 
         tour = self.__get_current_tour(self.__leagues[league_name])
         if tour is not None:
-            return len(tour["matches"])
+            return {
+                "number": int(tour["name"].split(" ")[0]),
+                "matches_count": len(tour["matches"]),
+                "deadline": iso8601.parse_date(tour["startedAt"]),
+            }
         else:
             return None
 
-    def get_cur_tour_deadline(self, league_name: str) -> t.Optional[datetime]:
-        if league_name not in self.__leagues:
-            logging.info(f"Wrong league_name={league_name} in get_deadline")
-            return None
-
-        tour = self.__get_current_tour(self.__leagues[league_name])
-        if tour is not None:
-            return iso8601.parse_date(tour["startedAt"])
-        else:
-            return None
-
-    def get_cur_tour_mathces_count(self, league_name: str) -> t.Optional[int]:
-        if league_name not in self.__leagues:
-            logging.info(f"Wrong league_name={league_name} in get_deadline")
-            return None
-
-        tour = self.__get_current_tour(self.__leagues[league_name])
-        if tour is not None:
-            return len(tour["matches"])
-        else:
-            return None
-
-    def get_tour_transfers(self, squad_id: int) -> t.Optional[bool]:
+    def get_tour_transfers(self, squad_id: int) -> Optional[bool]:
         data = self.__request_for_squad_id(squad_id, "squad")
         if data is None:
             return None
