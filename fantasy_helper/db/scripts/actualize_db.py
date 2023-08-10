e734188cc7898edb7ea6356370882ff92b19ec48
se4en <001122mj@gmail.com>
2023-08-10 16:55:18 +0300

    #12. Fix db error in cron

--- fantasy_helper/db/scripts/actualize_db.py
+++ fantasy_helper/db/scripts/actualize_db.py
@@ -6,7 +6,9 @@ import sys
 sys.path.insert(0, "/fantasy_helper")
 
 from fantasy_helper.db.dao.coeff import CoeffDAO
+from fantasy_helper.db.utils.create_db import create_db
 
 
 if __name__ == "__main__":
+    create_db()
     CoeffDAO().update_coeffs_all_leagues()
