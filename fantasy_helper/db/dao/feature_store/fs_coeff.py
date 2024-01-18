94562201965494c66952fd94329634a97ea28a6d
se4en <001122mj@gmail.com>
2024-01-18 23:13:29 +0300

    #28. Add feature store model for coeffs

---
+++ fantasy_helper/db/dao/feature_store/fs_coeff.py
@@ -0,0 +1,96 @@
+from typing import List, Literal
+from datetime import timezone
+
+from sqlalchemy import and_
+from sqlalchemy.orm import Session as SQLSession
+
+from fantasy_helper.db.models.feature_store.fs_coeff import FSCoeff
+from fantasy_helper.db.database import Session
+from fantasy_helper.utils.dataclasses import MatchInfo
+
+
+utc = timezone.utc
+
+
+class CoeffDAO:
+    def get_coeffs(
+        self, league_name: str, tour: Literal["cur", "next"]
+    ) -> List[MatchInfo]:
+        """
+        Retrieves the coefficients for a given league and tour.
+
+        Args:
+            league_name (str): The name of the league.
+            tour (Literal["cur", "next"]): The tour, either "cur" for the current tour or "next" for the next tour.
+
+        Returns:
+            List[MatchInfo]: A list of MatchInfo objects representing the coefficients for the given league and tour.
+        """
+        db_session: SQLSession = Session()
+
+        cur_tour_matches = (
+            db_session.query(FSCoeff)
+            .filter(and_(FSCoeff.league_name == league_name, FSCoeff.tour == tour))
+            .all()
+        )
+
+        db_session.commit()
+        db_session.close()
+
+        result = [
+            MatchInfo(
+                url=match.url,
+                league_name=match.league_name,
+                home_team=match.home_team,
+                away_team=match.away_team,
+                start_datetime=match.start_datetime,
+                total_1_over_1_5=match.total_1_over_1_5,
+                total_2_over_1_5=match.total_2_over_1_5,
+                total_1_under_0_5=match.total_1_under_0_5,
+                total_2_under_0_5=match.total_2_under_0_5,
+            )
+            for match in cur_tour_matches
+        ]
+
+        return result
+
+    def update_coeffs(
+        self, league_name: str, tour: Literal["cur", "next"], matches: List[MatchInfo]
+    ) -> None:
+        """
+        Update the coefficients for a given league and tour with new matches.
+
+        Args:
+            league_name (str): The name of the league.
+            tour (Literal["cur", "next"]): The tour to update the coefficients for.
+            matches (List[MatchInfo]): The list of new matches.
+
+        Returns:
+            None: This function does not return anything.
+        """
+        db_session: SQLSession = Session()
+
+        # remove all previous matches
+        db_session.query(FSCoeff).filter(
+            and_(FSCoeff.league_name == league_name, FSCoeff.tour == tour)
+        ).delete()
+
+        # add new matches
+        for match in matches:
+            db_session.add(
+                FSCoeff(
+                    home_team=match.home_team,
+                    away_team=match.away_team,
+                    league_name=league_name,
+                    tour=tour,
+                    start_datetime=match.start_datetime,
+                    url=match.url,
+                    total_1_over_1_5=match.total_1_over_1_5,
+                    total_2_over_1_5=match.total_2_over_1_5,
+                    total_1_under_0_5=match.total_1_under_0_5,
+                    total_2_under_0_5=match.total_2_under_0_5,
+                )
+            )
+
+        db_session.commit()
+        db_session.close()
---
+++ fantasy_helper/db/models/feature_store/fs_coeff.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+import typing as t
+
+from sqlalchemy import Column
+from sqlalchemy import Integer, String, DateTime, Float
+
+from fantasy_helper.db.database import Base
+
+
+class FSCoeff(Base):
+    __tablename__ = "fs_coeffs"
+
+    id = Column(Integer, primary_key=True)
+    home_team = Column(String, nullable=False)
+    away_team = Column(String, nullable=False)
+    league_name = Column(String, nullable=False)
+    tour = Column(String, nullable=False)
+    start_datetime = Column(DateTime, nullable=False)
+    url = Column(String, nullable=True)
+    total_1_over_1_5 = Column(Float, nullable=True)
+    total_1_under_0_5 = Column(Float, nullable=True)
+    total_2_over_1_5 = Column(Float, nullable=True)
+    total_2_under_0_5 = Column(Float, nullable=True)
+
+    def __init__(
+        self,
+        home_team: str,
+        away_team: str,
+        league_name: str,
+        tour: str,
+        start_datetime: datetime,
+        url: t.Optional[str],
+        total_1_over_1_5: t.Optional[float],
+        total_1_under_0_5: t.Optional[float],
+        total_2_over_1_5: t.Optional[float],
+        total_2_under_0_5: t.Optional[float],
+    ):
+        self.home_team = home_team
+        self.away_team = away_team
+        self.league_name = league_name
+        self.tour = tour
+        self.start_datetime = start_datetime
+        self.url = url
+        self.total_1_over_1_5 = total_1_over_1_5
+        self.total_1_under_0_5 = total_1_under_0_5
+        self.total_2_over_1_5 = total_2_over_1_5
+        self.total_2_under_0_5 = total_2_under_0_5
+
+    def __repr__(self):
+        return f"{self.home_team} vs {self.away_team} [{self.start_datetime.isoformat(timespec='hours')}]"
