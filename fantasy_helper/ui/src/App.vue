605ef2d60befd4ed032726a6f0526ec166b6aeb3
se4en <001122mj@gmail.com>
2025-03-13 21:44:46 +0000

    #115. Add basic vue js app

---
+++ fantasy_helper/ui/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+// A "ref" is a reactive data source that stores a value.
+// Technically, we don't need to wrap the string with ref()
+// in order to display it, but we will see in the next
+// example why it is needed if we ever intend to change
+// the value.
+const message = ref('Hello World!')
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<!-- <style>
+html, body {
+  background-color: #a83fcb!important;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style> -->
